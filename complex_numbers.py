9f2cd4c33c9155bc48524fd28ed8f7cbeb2e7a24
middo1 <hamidadeshina1@gmail.com>
2024-05-04 01:41:41 +0100

    Done addition, subtraction and multriplication

--- complex_numbers.py
+++ complex_numbers.py
@@ -14,5 +14,23 @@ class complex_number:
         return True
 
     def __str__(self) -> str:
-        return str(self.real) + ' + ' + str(self.imag) + "i"
-     
+        if self.real == 0:
+            return str(self.imag) + 'i'
+        if self.imag == 0:
+            return str(self.real)
+        return str(self.real) + " + " + str(self.imag) + "i"
+    
+    def __add__(self, other_complex_number):
+        if type(other_complex_number) != complex_number:
+            raise Exception("You can not add an invalid complex number")
+        return complex_number(self.real + other_complex_number.real, self.imag + other_complex_number.imag)
+    
+    def __sub__(self, other_complex_number):
+        if type(other_complex_number) != complex_number:
+            raise Exception("You can not subtract an invalid complex number")
+        return complex_number(self.real - other_complex_number.real, self.imag - other_complex_number.imag)
+    
+    def __mul__(self , other_complex_number):
+        if type(other_complex_number) != complex_number:
+            raise Exception("You can not muiltiply an invalid complex number")
+        return complex_number(self.real * other_complex_number.imag - other_complex_number.real * self.imag, self.imag * other_complex_number.real + other_complex_number.imag * self.real)
