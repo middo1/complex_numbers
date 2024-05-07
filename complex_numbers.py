99ea312f9e5b9d7c0c6ddf02ef874f4564a0c3e7
middo1 <hamidadeshina1@gmail.com>
2024-05-07 20:29:13 +0100

    Chore: division in progress

--- complex_numbers.py
+++ complex_numbers.py
@@ -33,8 +33,10 @@ class ComplexNumber:
         return ComplexNumber(self.real - other_complex_number.real, self.imag - other_complex_number.imag)
     
     def __mul__(self , other_complex_number):
-        if type(other_complex_number) != ComplexNumber:
+        if type(other_complex_number) not in [int, ComplexNumber]:
             raise Exception("You can not muiltiply an invalid complex number")
+        if type(other_complex_number) == int:
+            return ComplexNumber(other_complex_number * self.real, other_complex_number * self.imag)
         return ComplexNumber(self.real * other_complex_number.real - other_complex_number.imag * self.imag, self.imag * other_complex_number.real + other_complex_number.imag * self.real)
     
     def conjugate(self):
@@ -42,6 +44,16 @@ class ComplexNumber:
 
     def abs(self):
         return (self * self.conjugate()).real**0.5
+    
+    def __div__(self, dividend):
+        if type(dividend) not in [ComplexNumber]:
+            raise Exception("You can not divide a complex number using anything other than an integer and a complex number")
+        return self * dividend.conjugate() / dividend * dividend.conjugate()
+    
+    @staticmethod
+    def divide_by(value1: 'int|ComplexNumber', value2: 'int|ComplexNumber') -> 'ComplexNumber':
+        return value2.conjugate() * value1 / value2 * value2.conjugate
+
     
     def __pow__(self, power):
         if self.real == 0:
